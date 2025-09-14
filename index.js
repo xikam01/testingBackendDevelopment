74cd74645cc7ee4d04fea28e00a5890f36c5af36
xikam01 <cabdiraxmaanxikam190@gmail.com>
2025-09-14 14:35:51 -0700

    beginning

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const App = express();
+// const cors = require("cors");
+dotenv.config();
+
+App.use(express.json());
+const Port = process.env.Port || 2000;
+App.listen(Port, () => console.log(`Server Is Runing on ${Port}`));
---
+++ prisma/migrations/20250914161553_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "public"."user" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_username_key" ON "public"."user"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "public"."user"("email");
