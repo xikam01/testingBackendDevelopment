3c5f19851030497926f7d998b133b0d783706b52
xikam01 <cabdiraxmaanxikam190@gmail.com>
2025-09-14 17:17:45 -0700

    (Application Programming Interfaces

--- src/controllers/userCreate.js
+++ src/controllers/userCreate.js
@@ -1,5 +1,13 @@
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient, prisma } = require("@prisma/client");
 const prisma = new PrismaClient();
+const bcrypt = require("bcryptjs");
+const Jwt = require("jsonwebtoken");
+
+const generateToken = (user) => {
+  return Jwt.sign({ user }, process.env.Jwt_Sec, {
+    expiresIn: "4d",
+  });
+};
 
 const Registertionuser = async (req, res) => {
   try {
@@ -10,31 +18,77 @@ const Registertionuser = async (req, res) => {
         isSuccess: false,
       });
     }
+    //cheking email repiting
+    const chekingEmail = await prisma.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
 
-const chekingEmail = await prisma.user.findFirst({
-  where: {
-    email: email,
-  },
-});
-
-  if(chekingEmail) {
-    return res.status(400).json({ Message: "email is alredy using" });
-  }
-
-  const usercreate = await prisma.user.create({
-    data:{
-        username:username,
-        email:email,
-        password:password
+    if (chekingEmail) {
+      return res.status(400).json({ Message: "email is alredy using" });
     }
-  })
-  res.json({
-    user: { ...usercreate, Message:"Success"},
-  });
 
+    //  secret passwword
+
+    const salt = bcrypt.genSaltSync(11);
+    const hash = bcrypt.hashSync(password, salt);
+
+    const usercreate = await prisma.user.create({
+      data: {
+        username: username,
+        email: email,
+        password: hash,
+      },
+    });
+    res.json({
+      user: { ...usercreate, Message: "Success" },
+    });
   } catch (error) {
-    console.log()
+    console.log();
   }
+};
+
+
+
+
+//====================================================<<login>==========(⊙_⊙;)
+const userLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(404).json({ Message: "provide information" });
+    }
+
+    const sameEmail = await prisma.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!sameEmail) {
+      return res.status(400).json({ Message: " worng credentailsC" });
+    }
+
+    const unhiddenpassword = bcrypt.compareSync(password, user.password);
+
+    if (unhiddenpassword) {
+      const Token = generateToken(user.id);
+
+      const result = {
+        ID: user.id,
+        USERNAME: user.username,
+        EMAIL: user.email,
+        Token,
+      };
+
+      res.json.status(200).json({
+        Message: "You are login",
+        result: { ...result },
+      });
+    }
+  } catch (error) {}
 };
 
 
@@ -42,6 +96,7 @@ const chekingEmail = await prisma.user.findFirst({
 
 
 
-module.exports={
-    Registertionuser
-}
+module.exports = {
+  Registertionuser,
+  userLogin,
+};
